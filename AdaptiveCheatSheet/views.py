1f43b7a2507655b67dbfb1fa603611c289eac2e6
prit_sheth <pritsheth99@gmail.com>
2017-11-22 03:05:46 -0700

    Adding noe tags from notes request

--- AdaptiveCheatSheet/views.py
+++ AdaptiveCheatSheet/views.py
@@ -1,18 +1,30 @@
+import datetime
+import json
+from django.db import connection
+from django.db.models import *
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.template import loader
+from django.views.generic.base import RedirectView
+
 from AdaptiveCheatSheet.forms import *
 from AdaptiveCheatSheet.models import *
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.template import loader
-import datetime
-from django.db.models import *
-from django.db import connection
-from django.views.generic.base import RedirectView
 
 
 def save_note(request):
+    if 'application/json' in request.META['CONTENT_TYPE']:
+        data = json.loads(request.body)
+        #
+        # if request.method == 'GET':
+        #     request.GET = QueryDict(data)
+        #
+        # if request.method == 'POST':
+        #     request.POST = QueryDict(data)
+
     print(request.method)
     print(request.POST)
+    print(data)
     username = request.session['username']
     print(username)
     n = NotesForm()
@@ -20,36 +32,39 @@ def save_note(request):
         print('True')
     return 0
 
+
 def favicon_view(request):
     RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 
 def contact_us(request):
     temp = loader.get_template('contact_us.html')
-    return HttpResponse(temp.render({},request))
+    return HttpResponse(temp.render({}, request))
+
 
 def logout(request):
     temp = loader.get_template('login.html')
-    return HttpResponse(temp.render({},request))
+    return HttpResponse(temp.render({}, request))
+
 
 def login(request):
     if request.method == 'GET':
-        return render(request,'login.html',{})
-    elif request.method =='POST':
+        return render(request, 'login.html', {})
+    elif request.method == 'POST':
         f = LoginForm(request.POST)
         user_enter = LoginForm()
         if f.is_valid():
             user_enter.username = f.cleaned_data['username']
-            if User.objects.all().filter(username = user_enter.username) :
-                request.session['username']= user_enter.username
+            if User.objects.all().filter(username=user_enter.username):
+                request.session['username'] = user_enter.username
                 username = request.session['username']
-                temp  = loader.get_template('index.html')
+                temp = loader.get_template('index.html')
                 context = {
-                       'Username' : username
+                    'Username': username
                 }
 
                 # ----------------------------Context Created-----------------------------#
-                return HttpResponse(temp.render(context , request))
+                return HttpResponse(temp.render(context, request))
             else:
                 return HttpResponse("<p> Username / Password not valid !!!  </p>")
         else:
@@ -57,8 +72,8 @@ def login(request):
 
 
 def register(request):
-    if request.method=='GET':
-        return render(request, 'register.html' , {})
+    if request.method == 'GET':
+        return render(request, 'register.html', {})
     elif request.method == 'POST':
         f = RegisterForm(request.POST)
         if f.is_valid():
@@ -69,6 +84,3 @@ def register(request):
             user_obj.tags = f.cleaned_data['tags']
             user_obj.save()
             return redirect('/login/')
-
-
-
