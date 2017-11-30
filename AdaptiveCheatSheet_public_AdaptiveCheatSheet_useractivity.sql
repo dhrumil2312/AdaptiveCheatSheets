09990e9349d5b9b50b5e87b5033cbb744fb07c06
Dhrumil Parmar <dmparmar@asu.edu>
2017-11-30 00:01:45 -0700

    User Acitivty

--- AdaptiveCheatSheet_public_AdaptiveCheatSheet_useractivity.sql
+++ AdaptiveCheatSheet_public_AdaptiveCheatSheet_useractivity.sql
@@ -10,3 +10,16 @@ INSERT INTO public.AdaptiveCheatSheet_useractivity (id, user_id, note_id, upvote
 INSERT INTO public.AdaptiveCheatSheet_useractivity (id, user_id, note_id, upvote, downvote, notes_shared, note_access, tags) VALUES (34, 1, 562, 1, 1, 1, 10, 'HTML');
 INSERT INTO public.AdaptiveCheatSheet_useractivity (id, user_id, note_id, upvote, downvote, notes_shared, note_access, tags) VALUES (35, 1, 563, 1, 1, 1, 10, 'SQL');
 INSERT INTO public.AdaptiveCheatSheet_useractivity (id, user_id, note_id, upvote, downvote, notes_shared, note_access, tags) VALUES (36, 1, 532, 1, 1, 1, 10, 'LOOP;');
+
+create table example as select id, upvote old , row_number() over(order by 1) new from "AdaptiveCheatSheet_notes";
+
+select * from "AdaptiveCheatSheet_notes";
+update "AdaptiveCheatSheet_notes" a set downvote = id;
+
+update "AdaptiveCheatSheet_notes" a set upvote = (select new from example b where  a.id = b.id) where exists (select 1 from example c where a.id = c.id)
+
+update "AdaptiveCheatSheet_notes" set upvote =  row_number() over(order by 1);
+
+
+update( select  upvote old , row_number() over(order by 1) new from "AdaptiveCheatSheet_notes") set old = new;
+
